0b295daa1030a8da9f6711a83c39f1cb05c78cc7
iwilloug <iwilloug@b642a0b7-b348-0410-9912-e4a34d632523>
2009-02-03 08:39:49 +0000

    remove old version

--- trunk/MPS/installers/ibm/was/mps/mcs/repository/bin/mcsAssetLoader.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem Assumed that this is run from the bin directory and that there is
-rem a ..\lib directory with the jar files in.
-
-set TOOLS_LIB=..\lib
-
-java -jar %TOOLS_LIB%\volantis-mcs-assetloader-client-cli.jar %*
-
